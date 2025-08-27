9466da6a78b1bca0fa9952aa2088f379afcd4b92
TristanLe07 <107384818+TristanLe07@users.noreply.github.com>
2025-08-27 10:25:23 +0800

    reuploading entire game
    game version has changed to godot 4

---
+++ New Game Project/Scripts/Menus/MainMenu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+
+func _on_PlayButton_pressed():
+	pass # Replace with function body.
+
+
+func _on_SettingsButton_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Menus/Settings.tscn")
+
+
+func _on_ExitButton_pressed():
+	get_tree().quit()
