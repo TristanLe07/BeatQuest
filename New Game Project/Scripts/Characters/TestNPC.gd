80efda3bbab37e89b48b3b0d656cca1ff91d9f75
Siypr <68062506+SiyprYT@users.noreply.github.com>
2025-09-07 21:40:30 +0800

    INTERACTIONS EXIST!

--- New Game Project/Scripts/Characters/TestNPC.gd
+++ New Game Project/Scripts/Characters/TestNPC.gd
@@ -1,11 +1,9 @@
-extends Node2D
+extends StaticBody2D
 
-@onready var interaction_area: InteractionArea = $InteractionArea
-@onready var sprite = $Icon
+@onready var interactable: Area2D = $Interactable
 
-
-func _ready():
-	interaction_area.interact = Callable(self, "_on_interact")
+func _ready() -> void:
+	interactable.interact = _on_interact
 	
 func _on_interact():
 	get_tree().change_scene_to_file("res://Scenes/Menus/MainMenu.tscn")
