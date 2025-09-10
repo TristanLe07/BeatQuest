a9561781abe8cabb33b155198e8803e81f67517a
Siypr <68062506+SiyprYT@users.noreply.github.com>
2025-09-10 13:01:22 +0800

    scene transitions are done
    create map,
    create npc thingy
    create dialogue amaby
    where the hell is the rhythm things

--- New Game Project/Scenes/World/room_1.gd
+++ New Game Project/Scenes/World/room_1.gd
@@ -14,21 +14,16 @@ func _process(delta):
 func change_scene():
 	if Global.transition_scene == true:
 		if Global.current_scene == "world":
-			Global.finish_changescenes()
 			Global.game_first_loading = false
+			Global.finish_changescenes()
 			get_tree().change_scene_to_file("res://Scenes/World/room_2.tscn")
 
 
 
 func _on_room_2_entrance_body_exited(body):
-	print("exited")
 	if body is Player:
-		print("exited")
 		Global.transition_scene = false
 
 func _on_room_2_entrance_body_entered(body):
-	print("entered")
 	if body is Player:
-		print("entered")
 		Global.transition_scene = true
-		print(Global.transition_scene)
--- New Game Project/Scripts/Global/global.gd
+++ New Game Project/Scripts/Global/global.gd
@@ -3,8 +3,8 @@ extends Node
 var current_scene = "world" #MAIN AREA I THINK
 var transition_scene = false
 
-var player_exit_main_posx = 432
-var player_exit_main_posy = 64
+var player_exit_main_posx = 216
+var player_exit_main_posy = 72
 var player_start_posx = 300
 var player_start_posy = 100
 
@@ -17,7 +17,6 @@ func finish_changescenes():
 		print(transition_scene)
 		if current_scene == "world":
 			current_scene = "Room2"
-			print("CHANGINGE SCENE")
 		else:
 			print("why is the scene changing back to world")
 			current_scene = "world"
