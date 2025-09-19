ee047e33c61efcf69405f32ddb1bc1843ce05e10
Siypr <68062506+SiyprYT@users.noreply.github.com>
2025-09-20 00:20:31 +0800

    Delete New Game Project directory

--- New Game Project/Scripts/world/Cutscene/cutscene.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-func _ready():
-	$AnimationPlayer.play("Test")
-
-func _on_animation_player_animation_finished(anim_name):
-	get_tree().change_scene_to_file("res://Scenes/World/room_1.tscn")
--- New Game Project/Scripts/world/Interactions/interactable.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-class_name Interactable
-
-@export var interact_name: String = ""
-@export var is_interactable: bool = true
-
-var interact: Callable = func():
-	pass
